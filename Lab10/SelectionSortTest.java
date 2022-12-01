6235f05deadebbec8f25247fd097cf8ebce41843
Jacob Marchywka <dcfalcons2021@gmail.com>
2022-12-01 10:11:44 -0500

    Initial tests

---
+++ Lab10/SelectionSortTest.java
@@ -0,0 +1,108 @@
+package Lab10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SelectionSortTest {
+
+	@Test
+	void testPositive() {
+		
+		int[] arr = new int[5];
+        arr[0] = 8;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 2;
+        Sortedarr[1] = 7;
+        Sortedarr[2] = 8;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
+        
+        SelectionSort temp = new SelectionSort();
+        int[] selSortarr = new int[5];
+        selSortarr = temp.basicSelectionSort(arr);
+        
+        assertEquals(Sortedarr, selSortarr);
+		fail("All positive failed");
+	}
+	@Test
+	void TestNegative() {
+		
+			
+			int[] arr = new int[5];
+	        arr[0] = -8;
+	        arr[1] = -9;
+	        arr[2] = -7;
+	        arr[3] = -10;
+	        arr[4] = -2;
+	        
+	        int[] Sortedarr = new int[5];
+	        Sortedarr[0] = -10;
+	        Sortedarr[1] = -9;
+	        Sortedarr[2] = -8;
+	        Sortedarr[3] = -7;
+	        Sortedarr[4] = -2;
+	        
+	        SelectionSort temp = new SelectionSort();
+	        int[] selSortarr = new int[5];
+	        selSortarr = temp.basicSelectionSort(arr);
+	        
+	        assertEquals(Sortedarr, selSortarr);
+	        fail("All Negative failed");
+	}
+	
+	@Test
+	void TestMixed() {
+		int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = 7;
+        arr[3] = -10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = 7;
+        Sortedarr[4] = 2;
+        
+        SelectionSort temp = new SelectionSort();
+        int[] selSortarr = new int[5];
+        selSortarr = temp.basicSelectionSort(arr);
+        
+        assertEquals(Sortedarr, selSortarr);
+        fail("Mixed Numbers failed");
+	}
+	
+	@Test
+	void TestDuplicates() {
+		int[] arr = new int[5];
+        arr[0] = 2;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = 2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = 2;
+        Sortedarr[4] = 2;
+        
+        SelectionSort temp = new SelectionSort();
+        int[] selSortarr = new int[5];
+        selSortarr = temp.basicSelectionSort(arr);
+        
+        assertEquals(Sortedarr, selSortarr);
+        fail("Test Duplicates failed");
+	}
+	
+
+}
